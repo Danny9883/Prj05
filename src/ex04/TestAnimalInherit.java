ee169afef3bcb33fc14ed307448587be1ba8c37a
danny9883 <danny9883@naver.com>
2026-03-25 14:53:23 +0900

    Pet class / abstract 활용예제

--- src/ex04/TestAnimalInherit.java
+++ src/ex04/TestAnimalInherit.java
@@ -20,29 +20,89 @@ class Cat extends Pet {
 	}
 }
 
+class Tiger extends Pet {
+	void    cry() {
+		System.out.println(name + " (이)가 어흥흥");
+	}
+}
+
 public class TestAnimalInherit {
 	
 	// 상속 : Inherit -> java 에서는 extends 로 처리
 	// 다중상속 불가능 (상속은 한개만 가능) -> Java, C#
 	public static void main(String[] args) {
 		
+		Dog dog  = new Dog();
+		work(dog,"츄");
+//		dog.name = "츄";
+//		dog.eat();
+//		dog.bark();
+		System.out.println("========================");
+		
+		// 가능
+//		pet     =  dog;
+//		pet.eat();
+		// pet.bark(); // 불가능
+		
+		// 불가능 (자식 = 부모) -> casting 을 하면 가능
+		// dog      = pet;
+		// dog.eat();
+		// dog      = (Dog) pet;
+		// dog.eat();
+		
+		
+		
+		System.out.println("========================");
+		Cat nero  = new Cat();
+		work(nero,"네로");
+//		nero.name = "네로";
+//		nero.eat();
+//		nero.meow();
+		System.out.println("========================");
+		
+		
+		Tiger tiger = new Tiger();
+		work(tiger,"호냥이");
+		
+		
+		System.out.println("========================");
 		Pet pet  = new Pet();
-		pet.name = "구피";
+		pet.name = "뻐끔스";
+		pet.eat();
+
+
+	}
+
+	// 한개의 work() 함수로 업무처리
+	private static void work(Pet pet, String name) {
+		pet.name = name;
+		System.out.println("Work : ");
 		pet.eat();
 		
+		if ( pet instanceof Dog )
+			((Dog) pet).bark();
+		if ( pet instanceof Cat )
+			((Cat) pet).meow();
+		if ( pet instanceof Tiger )
+			((Tiger) pet).cry();
 		
-		Dog dog  = new Dog();
-		dog.name = "츄";
-		dog.eat();
-		dog.bark();
-		
-		
-		Cat nero  = new Cat();
-		nero.name = "네로";
+	}
+	
+
+/*	private static void work(Cat nero, String name) {
+		nero.name = name;
+		System.out.print("work : ");
 		nero.eat();
 		nero.meow();
 		
-
 	}
+
+	private static void work(Dog dog, String name) {
+		dog.name = name;
+		System.out.print("work : ");
+		dog.eat();
+		dog.bark();
+		
+	}  */
 
 }
